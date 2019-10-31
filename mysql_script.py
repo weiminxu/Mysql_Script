832709623df7ef01dfa5f33ca867cb491c88249d
weiminxu <weiminxu1982@gmail.com>
2019-10-31 23:22:52 +1300

    select script
    select script

--- mysql_script.py
+++ mysql_script.py
@@ -20,21 +20,28 @@ mycursor = mydb.cursor()
 #val = ("John", "Highway 21")
 #mycursor.execute(sql, val)
 
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = [
-    ("Peter", "Low street 4"),
-    ("Amy", "Apple street 652"),
-    ("Hannah", "Mountain 21"),
-    ("Michael", "Valley 345"),
-    ("Sandy", "Ocean blvd 2"),
-    ("Betty", "Green Grass 1"),
-    ("Richard", "Sky st 331"),
-    ('Susan', 'One way 98'),
-    ('Vicky', 'Yellow Garden 2'),
-    ('Ben', 'Park Lane 38'),
-    ('William', 'Central st 954'),
-    ('Chuck', 'Main Road 989'),
-    ('Viola', 'Sideway 1633')
-]
-mycursor.executemany(sql, val)
-mydb.commit()
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = [
+#     ("Peter", "Low street 4"),
+#     ("Amy", "Apple street 652"),
+#     ("Hannah", "Mountain 21"),
+#     ("Michael", "Valley 345"),
+#     ("Sandy", "Ocean blvd 2"),
+#     ("Betty", "Green Grass 1"),
+#     ("Richard", "Sky st 331"),
+#     ('Susan', 'One way 98'),
+#     ('Vicky', 'Yellow Garden 2'),
+#     ('Ben', 'Park Lane 38'),
+#     ('William', 'Central st 954'),
+#     ('Chuck', 'Main Road 989'),
+#     ('Viola', 'Sideway 1633')
+# ]
+# mycursor.executemany(sql, val)
+# mydb.commit()
+
+mycursor.execute("SELECT * FROM customers")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(x)
