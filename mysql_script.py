10e1c6ccb4daffaf7825d179faf31c33ac989491
weiminxu <weiminxu1982@gmail.com>
2019-10-31 23:46:00 +1300

    order script
    order script

--- mysql_script.py
+++ mysql_script.py
@@ -44,7 +44,13 @@ mycursor = mydb.cursor()
 # for x in myresult:
 #     print(x)
 
-sql = "SELECT * FROM customers WHERE address = 'Park Lane 38'"
+# sql = "SELECT * FROM customers WHERE address = 'Park Lane 38'"
+# mycursor.execute(sql)
+# myresult = mycursor.fetchall()
+# for x in myresult:
+#     print(x)
+
+sql = "SELECT * FROM customers ORDER BY name"
 mycursor.execute(sql)
 myresult = mycursor.fetchall()
 for x in myresult:
