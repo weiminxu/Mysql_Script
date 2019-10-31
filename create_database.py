d32e8f2918a2813112fcb164ce3c14e414d9b7a0
weiminxu <weiminxu1982@gmail.com>
2019-10-31 15:59:02 +1300

    create table
    create table

--- create_database.py
+++ create_database.py
@@ -3,7 +3,8 @@ import mysql.connector  #python -m pip install mysql-connector
 mydb = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
-    passwd="123456")
+    passwd="123456",
+    database="mysql_script")
 
 #auth_plugin="mysql_native_password"
 
@@ -11,5 +12,6 @@ print(mydb)
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE MySQLScript")
+#mycursor.execute("CREATE DATABASE mysql_script")
 
+mycursor.execute("CREATE TABLE customers(name VARCHAR(255), address VARCHAR(255))")
